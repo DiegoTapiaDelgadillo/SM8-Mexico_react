228a1ccf35def36126c5f80a0c9c31d0e8e46106
david-cornejo <davidcg914@gmail.com>
2025-02-13 12:58:46 -0600

    navbar mobile animation

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,7 +6,7 @@ import Linkedin from "./img/linkedin.png";
 import Whastapp from "./img/whatsapp.png";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from "../../context/AuthContext";
 
 export default function Navbar() {
   const [isNavbarOpen, setNavbarOpen] = useState(false);
@@ -110,7 +110,11 @@ export default function Navbar() {
                   </a>
                 </li>
                 <li className="px-2 py-4 cursor-pointer hover:brightness-50 ease-in-out duration-300">
-                  <a href=" https://wa.me/5215514231881" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href=" https://wa.me/5215514231881"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <img src={Whastapp} alt="WhatsApp" className="w-6" />
                   </a>
                 </li>
@@ -128,7 +132,7 @@ export default function Navbar() {
                     className="ml-4 text-red-500 hover:text-red-700 font-bold py-2 px-4 rounded"
                   >
                     Cerrar Sesión
-                  </button>                  
+                  </button>
                 </div>
               )}
             </div>
@@ -183,126 +187,131 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
-      <div className={isNavbarOpen ? "block xl:hidden" : "hidden"}>
-        <div className="grid pt-16 w-full h-screen bg-neutral-900 text-yellow-300 justify-items-center fixed z-40">
-          <div className="grid w-3/6 pt-8 pb-8">
-            <div className="grid w-full">
-              <ul className="divide-y-2 divide-yellow-300/10">
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <Link to={"/"} onClick={NavbarOpen}>
-                    Inicio
-                  </Link>
-                </li>
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <Link to={"/Servicios"} onClick={NavbarOpen}>
-                    Servicios
-                  </Link>
-                </li>
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <Link to={"/Sucursales"} onClick={NavbarOpen}>
-                    Sucursales
-                  </Link>
-                </li>
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <Link to={"/Vacantes"} onClick={NavbarOpen}>
-                    Vacantes
-                  </Link>
-                </li>
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <Link to={"/Contacto"} onClick={NavbarOpen}>
-                    Contacto
-                  </Link>
-                </li>
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <Link to={"/Nosotros"} onClick={NavbarOpen}>
-                    Nosotros
-                  </Link>
-                </li>
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <Link to={"/Noticias"} onClick={NavbarOpen}>
-                    Noticias
-                  </Link>
-                </li>
-                <li className="text-yellow-300 cursor-pointer hover:text-yellow-300/75 ease-in-out duration-300 py-4">
-                  <a
-                    href="https://publuu.com/flip-book/177837/438154"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Catálogo
-                  </a>
-                </li>
-                <li className="py-8">
-                  <div className="flex w-full justify-center">
-                    <div className="px-2">
-                      <a
-                        href="https://www.facebook.com/sm8demexico"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <img
-                          src={Facebook}
-                          alt="Facebook"
-                          className="w-8 cursor-pointer hover:brightness-50 ease-in-out duration-300"
-                        />
-                      </a>
-                    </div>
-                    <div className="px-2">
-                      <a
-                        href="https://www.instagram.com/sm8demexico/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <img
-                          src={Instagram}
-                          alt="Instagram"
-                          className="w-8 cursor-pointer hover:brightness-50 ease-in-out duration-300"
-                        />
-                      </a>
-                    </div>
-                    <div className="px-2">
-                      <a
-                        href="https://www.linkedin.com/company/sm8/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <img
-                          src={Linkedin}
-                          alt="LinkedIn"
-                          className="w-8 cursor-pointer hover:brightness-50 ease-in-out duration-300"
-                        />
-                      </a>
-                    </div>
-                    <div className="px-2">
-                      <a href=" https://wa.me/5215514231881" target="_blank" rel="noopener noreferrer">
-                        <img
-                          src={Whastapp}
-                          alt="WhatsApp"
-                          className="w-8 cursor-pointer hover:brightness-50 ease-in-out duration-300"
-                        />
-                      </a>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-              {token && (
-                <div className="flex items-center">
-                  <Link
-                    to="/gestion-do"
-                    className="ml-4 text-yellow-300 hover:text-yellow-500 font-bold py-2 px-4 rounded"
-                  >
-                    DO
-                  </Link>
-                  <button
-                    onClick={logout}
-                    className="ml-4 text-red-500 hover:text-red-700 font-bold py-2 px-4 rounded"
-                  >
-                    Cerrar Sesión
-                  </button>                  
-                </div>
-              )}
-            </div>  
+      <div
+        className={`xl:hidden fixed top-0 right-0 w-full h-screen z-40 transition-transform duration-300 transform ${
+          isNavbarOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="grid pt-16 w-full h-screen bg-neutral-900 text-yellow-300 justify-center items-center">
+          <ul className="w-full text-center divide-y-2 divide-yellow-300/10">
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <Link to="/" onClick={NavbarOpen}>
+                Inicio
+              </Link>
+            </li>
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <Link to="/Servicios" onClick={NavbarOpen}>
+                Servicios
+              </Link>
+            </li>
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <Link to="/Sucursales" onClick={NavbarOpen}>
+                Sucursales
+              </Link>
+            </li>
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <Link to="/Vacantes" onClick={NavbarOpen}>
+                Vacantes
+              </Link>
+            </li>
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <Link to="/Contacto" onClick={NavbarOpen}>
+                Contacto
+              </Link>
+            </li>
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <Link to="/Nosotros" onClick={NavbarOpen}>
+                Nosotros
+              </Link>
+            </li>
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <Link to="/Noticias" onClick={NavbarOpen}>
+                Noticias
+              </Link>
+            </li>
+            <li className="py-4 hover:text-yellow-300/75 transition-colors duration-300">
+              <a
+                href="https://publuu.com/flip-book/177837/438154"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={NavbarOpen}
+              >
+                Catálogo
+              </a>
+            </li>
+          </ul>
+
+          <div className="flex justify-center mt-8">
+            <div className="px-2">
+              <a
+                href="https://www.facebook.com/sm8demexico"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={Facebook}
+                  alt="Facebook"
+                  className="w-8 cursor-pointer hover:brightness-50 transition-all duration-300"
+                />
+              </a>
+            </div>
+            <div className="px-2">
+              <a
+                href="https://www.instagram.com/sm8demexico/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={Instagram}
+                  alt="Instagram"
+                  className="w-8 cursor-pointer hover:brightness-50 transition-all duration-300"
+                />
+              </a>
+            </div>
+            <div className="px-2">
+              <a
+                href="https://www.linkedin.com/company/sm8/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={Linkedin}
+                  alt="LinkedIn"
+                  className="w-8 cursor-pointer hover:brightness-50 transition-all duration-300"
+                />
+              </a>
+            </div>
+            <div className="px-2">
+              <a
+                href="https://wa.me/5215514231881"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={Whastapp}
+                  alt="WhatsApp"
+                  className="w-8 cursor-pointer hover:brightness-50 transition-all duration-300"
+                />
+              </a>
+            </div>
           </div>
+
+          {token && (
+            <div className="flex items-center mt-8">
+              <Link
+                to="/gestion-do"
+                className="text-yellow-300 hover:text-yellow-500 font-bold py-2 px-4 rounded"
+              >
+                DO
+              </Link>
+              <button
+                onClick={logout}
+                className="ml-4 text-red-500 hover:text-red-700 font-bold py-2 px-4 rounded"
+              >
+                Cerrar Sesión
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
